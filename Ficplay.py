51d960044cfe0a3c5f50157ed2e1c1295d6efd6c
Naoya Sho <ut10.sho708@gmail.com>
2014-06-09 17:50:33 +0900

    test

---
+++ Ficplay.py
@@ -0,0 +1,14 @@
+%matplotlib inline  
+
+from pylab import plot, show, legend
+from random import normalvariate
+
+alpha = 0.9
+ts_length = 200
+current_x = 0
+
+x_values = []
+for i in range(ts_length):
+    x_values.append(current_x)
+    current_x = alpha * current_x + normalvariate(0, 1)
+plot(x_values, 'b-')
