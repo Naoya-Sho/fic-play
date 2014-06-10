61a0552c696f375bc186877c4cb149d1998ddfca
Naoya Sho <ut10.sho708@gmail.com>
2014-06-10 16:29:27 +0900

    preliminary version - plotting x0 and x1

--- Ficplay.py
+++ Ficplay.py
@@ -1,14 +1,69 @@
-%matplotlib inline  
+import matplotlib.pyplot as plt
+import random as random
+from __future__ import division
 
-from pylab import plot, show, legend
-from random import normalvariate
+fig, ax = plt.subplots()
 
-alpha = 0.9
-ts_length = 200
-current_x = 0
+players = [0,1]
 
-x_values = []
-for i in range(ts_length):
-    x_values.append(current_x)
-    current_x = alpha * current_x + normalvariate(0, 1)
-plot(x_values, 'b-')
+trials = 201
+current_x0 = 0.5
+current_x1 = 0.5
+
+# setting up the payoff function
+
+def pay0(a,b):
+    if a == b:
+        return 1
+    else:
+        return -1
+
+def pay1(a,b):
+    if a != b:
+        return 1
+    else:
+        return -1
+
+# best response function using the payoff function 
+def br0(x):
+    if x * pay0(0,0) + (1-x) * pay0(0,1) > x * pay0(1,0) + (1-x) * pay0(1,1):
+        return 0
+
+    elif x * pay0(0,0) + (1-x) * pay0(0,1) < x * pay0(1,0) + (1-x) * pay0(1,1):
+        return 1
+
+    else:
+        return random.randint(0,1)
+
+def br1(x):
+    if x * pay1(0,0) + (1-x) * pay1(1,0) > x * pay1(0,1) + (1-x) * pay1(1,1):
+        return 0
+
+    elif x * pay1(0,0) + (1-x) * pay1(1,0) < x * pay1(0,1) + (1-x) * pay1(1,1):
+        return 1
+
+    else:
+        return random.randint(0,1)
+
+# can be combined by using class?OOP later on
+
+
+x0_values = []
+x1_values = []
+xaxis = range(trials)
+
+for i in range(trials):
+    a0 = br0(current_x0)
+    a1 = br1(current_x1)
+
+    x0_values.append(current_x0)
+    current_x0 = current_x0 + (a1-current_x0)/(i + 2)
+
+    x1_values.append(current_x1)
+    current_x1 = current_x1 + (a0-current_x1)/(i + 2)
+
+ax.plot(xaxis, x0_values, 'b-')
+ax.plot(xaxis, x1_values, 'r-')
+plt.show()
+
+
