8f952256d79ef86359f138e592ba700a67b43288
Naoya Sho <ut10.sho708@gmail.com>
2014-06-14 16:33:38 +0900

    something is wrong with the histogram...

--- Ficplay.py
+++ Ficplay.py
@@ -4,9 +4,9 @@ from __future__ import division
 
 fig, ax = plt.subplots()
 
-players = [0,1]
+players = [0,1] # not used yet
 
-trials = 201
+trials = 51
 current_x0 = 0.5
 current_x1 = 0.5
 
@@ -47,10 +47,9 @@ def br1(x):
 
 # can be combined by using class?OOP later on
 
-
+## plot x0 and x1 and save the figure
 x0_values = []
 x1_values = []
-xaxis = range(trials)
 
 for i in range(trials):
     a0 = br0(current_x0)
@@ -62,8 +61,42 @@ for i in range(trials):
     x1_values.append(current_x1)
     current_x1 = current_x1 + (a0-current_x1)/(i + 2)
 
+
+xaxis = range(trials)
 ax.plot(xaxis, x0_values, 'b-')
 ax.plot(xaxis, x1_values, 'r-')
-plt.show()
+xaxis = range(trials)
+plt.savefig('Matpenny' + str(trials-1) + '.png',transparent=True, bbox_inches='tight', pad_inches=0)
+plt.close()
 
+###################
+## plot the histogram and save the figure
+# make a list of x0(T-1)
+
+iter = 200
+terminalx0 = []
+
+for j in range(iter + 1):
+    
+    terminalx0.append(x0_values[trials - 1])
+
+    x0_values = []
+    x1_values = []
+
+    for i in range(trials):
+        a0 = br0(current_x0)
+        a1 = br1(current_x1)
+
+        x0_values.append(current_x0)
+        current_x0 = current_x0 + (a1-current_x0)/(i + 2)
+
+        x1_values.append(current_x1)
+        current_x1 = current_x1 + (a0-current_x1)/(i + 2)
+
+# plot the list in histogram
+n, bins, patches = plt.hist(terminalx0, 15, normed = 1)
+plt.setp(patches, 'facecolor', 'g')
+
+plt.savefig('Matpenny_hist' + str(trials-1) +'_'+ str(iter) +'.png',transparent=True, bbox_inches='tight', pad_inches=0)
+plt.show()
 
